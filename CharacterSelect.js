1399180a5eae1d0554dd9603d4919cf014858c2b
Yowwowwow <76942268+Yowwowwow@users.noreply.github.com>
2024-07-07 03:36:53 +0800

    Implement save/load last character selection

--- CharacterSelect.js
+++ CharacterSelect.js
@@ -3,6 +3,21 @@ var pcount=4;
 const disps = [document.getElementById("cd0"), document.getElementById("cd1"), document.getElementById("cd2"), document.getElementById("cd3")];
 const boxes = [document.getElementById("cb0"), document.getElementById("cb1"), document.getElementById("cb2"), document.getElementById("cb3")];
 const pdisp = document.getElementById("playercount");
+var last=localStorage.getItem("chars");
+while(last!=null){
+    let you = parseInt(last[0]);
+    let otherthree = parseInt(last[1]);
+    if(!(you<=3&&you>=0))break;
+    if(!(otherthree<=7&&otherthree>=0))break;
+    selected=you;
+    ChangeYou(selected);
+    for(let i=0, j=0;i<4;i++){
+        if(i==you)continue;
+        boxes[i].checked=(otherthree.toString(2).padStart(3, '0')[j]=='1');
+        j++;
+    }
+break;}
+ChangeYou(selected);
 function ChangeYou(ind){
     for(let i=0;i<disps.length;i++){
         if(i==ind){
@@ -27,4 +42,13 @@ function CheckCharacter(event){
     pcount=tmp;
     pdisp.innerHTML = pcount.toString().concat((pcount==1)?" Player":" Players");
 }
-ChangeYou(selected);
+function SaveChars(){
+    let you = selected.toString();
+    let otherthree = 0;
+    for(let i=0, j=0;i<4;i++){
+        if(i==you)continue;
+        otherthree+=(boxes[i].checked?Math.pow(2, 2-j):0);
+        j++;
+    }
+    localStorage.setItem("chars", you.concat(otherthree.toString()));
+}
