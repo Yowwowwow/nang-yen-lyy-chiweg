21439701c1e8a8116725e092ea327f2c4b9d2b31
Yowwowwow <76942268+Yowwowwow@users.noreply.github.com>
2024-07-28 16:08:00 +0800

    Add volume adjust, how to play link

--- scripts/Settings.js
+++ scripts/Settings.js
@@ -2,18 +2,23 @@ var bg = document.getElementById("setbg");
 var bgm = document.getElementById("bgm");
 var sldbgm = document.getElementById("sldbgm");
 var sldsfx = document.getElementById("sldsfx");
+var tvbgm = document.getElementById("tvbgm");
+var tvsfx = document.getElementById("tvsfx");
 var volbgm=localStorage.getItem("volbgm"); if(volbgm==null || volbgm>100 || volbgm<0)volbgm=90;
 var volsfx=localStorage.getItem("volsfx"); if(volsfx==null || volsfx>100 || volsfx<0)volsfx=90;
-sldbgm.value = volbgm;
-sldsfx.value = volsfx;
-bgm.volume = volbgm/100;
-bgm.play().catch(()=>{/*console.log("Tried to play audio but can't yet.");*/});
 function Volbgmchange(){
     bgm.volume = sldbgm.value/100;
+    tvbgm.innerHTML = sldbgm.value;
+    localStorage.setItem("volbgm", sldbgm.value);
 }
 function Volsfxchange(){
-    
+    tvsfx.innerHTML = sldsfx.value;
+    localStorage.setItem("volsfx", sldsfx.value);
 }
+sldbgm.value = volbgm;
+sldsfx.value = volsfx;
+Volbgmchange(); Volsfxchange();
+bgm.play().catch(()=>{/*console.log("Tried to play audio but can't yet.");*/});
 function Openbg(){
     bg.style.visibility = "visible";
 }
