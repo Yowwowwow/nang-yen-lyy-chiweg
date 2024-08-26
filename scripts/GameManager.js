0bde0b68762fd8cc98eff85a66bb922ad7b79ae5
Yowwowwow <76942268+Yowwowwow@users.noreply.github.com>
2024-08-26 20:04:12 +0800

    Writing GameManager

---
+++ scripts/GameManager.js
@@ -0,0 +1,10 @@
+function GMNGStartRound(){
+    let bag = [0,1,1,2,2,3,3,4,4,5,5,6,6,6,6,6,10,11,11,12,12,13,13,14,14,15,15,16,16,16,16,16];
+    mtn = [];
+    for(let i=0;i<32;i++){
+        SetField(i, -1);
+        let tmp = CryRandint(bag.length);
+        mtn.push(bag[tmp]);
+        bag.splice(tmp, 1);
+    }
+}
