98010d0e9c8d4f8696b24d8041ab32e473105870
Yowwowwow <76942268+Yowwowwow@users.noreply.github.com>
2024-08-31 02:27:45 +0800

    Do most initialization of each jyu2

--- scripts/GameManager.js
+++ scripts/GameManager.js
@@ -1,3 +1,4 @@
+var dealtpieces;
 function GMNGStartRound(){
     let bag = [0,1,1,2,2,3,3,4,4,5,5,6,6,6,6,6,10,11,11,12,12,13,13,14,14,15,15,16,16,16,16,16];
     mtn = [];
@@ -7,5 +8,27 @@ function GMNGStartRound(){
         mtn.push(bag[tmp]);
         bag.splice(tmp, 1);
     }
-    //TODO: Complete initialization of each new shyun2
+    for(let i=0;i<moves.length;i++)moves[i].disabled=true;
+    for(let i=0;i<pnum;i++){
+        srrou[i] = [];
+        let j = charpos[i];
+        hands[j].innerHTML = "";
+        fulus[j].innerHTML = "";
+        draws[j].innerHTML = "";
+        draws[j].dataset.mov = 0;
+    }
+    //TODO: Complete initialization of each new jyu2 (UPDATE 2024AUG31: probably done, so far)
+    timeoutstoclear.push(setTimeout(()=>{dealtpieces=0;Give1Piece();}, 250));
+}
+function Give1Piece(){
+    if(dealtpieces>=pnum*4){return;}//Finish initial draw and let dealer start their turn
+    let tmp = mtn[0];
+    mtn.splice(0, 1);
+    SetField(dealtpieces, -2);
+    let target = (dealtpieces + gwind[1]) % pnum;
+    let isplayer = charpos[target]==0;
+    srrou[target].push(tmp);
+    hands[charpos[target]].innerHTML+=PieceOf(isplayer?tmp:-1);
+    dealtpieces++;
+    timeoutstoclear.push(lasttimeoutid = setTimeout(Give1Piece, 100));
 }
