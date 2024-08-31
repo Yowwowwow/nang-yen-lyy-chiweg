5004448ff0793b31322a914e2be2c8a523f217fb
Yowwowwow <76942268+Yowwowwow@users.noreply.github.com>
2024-09-01 00:53:13 +0800

    Now computers can play the first piece

--- scripts/GameManager.js
+++ scripts/GameManager.js
@@ -1,5 +1,6 @@
 var dealtpieces;
 var playedpieces;
+var sea; //list of played pieces
 function GMNGStartRound(){
     let bag = [0,1,1,2,2,3,3,4,4,5,5,6,6,6,6,6,10,11,11,12,12,13,13,14,14,15,15,16,16,16,16,16];
     mtn = [];
@@ -36,7 +37,7 @@ function DrawFromMtn(num=0){//pass 1 to this function to draw from tail, otherwi
 function Give1Piece(){
     if(dealtpieces>=pnum*4){
         turnp = gwind[1];
-        timeoutstoclear.push(setTimeout(()=>{playedpieces=0;TurnStart();}, 250));
+        timeoutstoclear.push(setTimeout(()=>{playedpieces=0;sea=[];TurnStart();}, 250));
         return;
     }//Finish initial draw and let dealer start their turn
     let tmp = DrawFromMtn();
@@ -50,8 +51,43 @@ function Give1Piece(){
 }
 function TurnStart(){
     if(mtn.length<=6-pnum){return;}//too few pieces left, liou2 jyu2
-    let tmp = DrawFromMtn();
+    let isplayer = turnp==pwind;
+    jin = DrawFromMtn();
     SetField(dealtpieces, -2);
     dealtpieces++;
-    
+    draws[charpos[turnp]].innerHTML = PieceOf(isplayer?jin:-1);
+    if(!isplayer){ComputerThinksPlay();}
+    else{
+        for(let i=0, arr=hands[charpos[turnp]].children;i<arr.length;i++)arr[i].onclick=()=>{PlayerPlays(i);};
+        draws[charpos[turnp]].children[0].onclick=()=>{PlayerPlays(4);};
+    }
+}
+function ComputerThinksPlay(){
+    //TODO: will need to implement kang&riichi&tsumo
+    let ans = Randint(srrou[turnp].length+1);
+    timeoutstoclear.push(setTimeout(()=>{ComputerPlays(ans);}, Randint(325, 675)));
+}
+function ComputerPlays(num){
+    let tmp;
+    if(num<srrou[turnp].length){//not muo1 chiye1
+        tmp = srrou[turnp][num];
+        hands[charpos[turnp]].children[Randint(srrou[turnp].length)].dataset.val = srrou[turnp][num];
+        srrou[turnp][num] = jin;
+    }
+    else{//this is muo1 chiye1
+        tmp = jin;
+        draws[charpos[turnp]].innerHTML = PieceOf(jin);
+    }
+    ttc(()=>{ComputerDonePlaying(tmp);}, 667);
+}
+function ComputerDonePlaying(num){
+    draws[charpos[turnp]].innerHTML = "";
+    for(let i=0, arr=hands[charpos[turnp]].children;i<arr.length;i++)arr[i].dataset.val=-1;
+    SetField(playedpieces, num, charpos[turnp]);
+    playedpieces++;
+    sea.push(num);
+    //TODO: Now, check chi&pong&kang&ron before proceeding to the next player's turn
+}
+function PlayerPlays(num){
+    console.log(num);
 }
